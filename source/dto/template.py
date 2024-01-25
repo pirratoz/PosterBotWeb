a073d89463889d1a916bce229c26ca7b7a3721c5
PirraToZ <pirratoz@gmail.com>
2024-01-25 23:51:31 +0300

    feat(): add dto for media

--- source/dto/template.py
+++ source/dto/template.py
@@ -1,9 +1,22 @@
 from typing import Any
 
+from pydantic import BaseModel
+
 from source.dto.base_dto import BaseModelDto
+
+
+class MediaIds(BaseModel):
+    photo: list[int] | None = []
+    video: list[int] | None = []
+
+
+class Media(BaseModel):
+    text_message_id: int
+    media_ids: MediaIds
+    keyboard: list[list[dict[str, Any]]] | None = []
 
 
 class TemplateDto(BaseModelDto):
     id: int
     from_chat_id: int
-    media: dict[str, Any]
+    media: Media
